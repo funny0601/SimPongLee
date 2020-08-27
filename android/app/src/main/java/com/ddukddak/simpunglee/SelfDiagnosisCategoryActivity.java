e2476e0aad8723600e8747b6fc0ee66929f13755
Seyoung Choi <tpdude0601@gmail.com>
2020-08-28 00:40:29 +0900

    자가진단 카테고리 선택 시작 화면 추가

--- android/app/src/main/java/com/ddukddak/simpunglee/SelfDiagnosisCategoryActivity.java
+++ android/app/src/main/java/com/ddukddak/simpunglee/SelfDiagnosisCategoryActivity.java
@@ -1,21 +1,28 @@
 package com.ddukddak.simpunglee;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SelfDiagnosisCategoryActivity extends AppCompatActivity {
 
-    String url = "http://192.168.56.1:8090/";
-
+    //String url = "http://192.168.56.1:8090/";
+    GridView gridView;
     int userid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
-        userid = getIntent().getIntExtra("userid", 0);
+        //userid = getIntent().getIntExtra("userid", 0);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_emoji, null);
+
+        final GridView gridView = (GridView)view.findViewById(R.id.category_list);
     }
 }
